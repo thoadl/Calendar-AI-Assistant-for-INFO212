8df7bfb71c3b96c8060b77b5d5facf29abad5a25
alicia.carretero <alicia.carretero@student.uib.no>
2025-10-12 13:51:27 +0200

    html updated

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, session, redirect
+from flask import Flask, request, jsonify, session, redirect, send_from_directory
 import os, json
 from datetime import datetime
 from flask_cors import CORS
@@ -7,10 +7,18 @@ from oop_events import CalendarManager  # tu clase con Event y CalendarManager
 from google_auth_oauthlib.flow import Flow
 
 # -------------------- CONFIG --------------------
-sync = GoogleCalendarSync("credentials.json", "token.json")
+sync = GoogleCalendarSync(
+    scopes=["https://www.googleapis.com/auth/calendar"],
+    credentials_file="credentials.json",
+    token_file="token.json"
+)
 calendar_manager = CalendarManager()  # maneja draft y real
+calendar_manager.real_calendar.load_from_file(calendar_manager.real_file)
+calendar_manager.draft_calendar.load_from_file(calendar_manager.draft_file)
+
 
 app = Flask(__name__)
+
 CORS(app)
 
 app.secret_key = "secret"  # change
@@ -19,6 +27,7 @@ SCOPES = ["https://www.googleapis.com/auth/calendar"]
 CLIENT_SECRETS_FILE = "credentials.json"
 
 # -------------------- ENDPOINTS --------------------
+
 @app.route("/auth/google")
 def auth_google():
     flow = Flow.from_client_secrets_file(
@@ -83,7 +92,10 @@ def get_events():
             "draft": False,
         })
     
-
+    '''
+    return jsonify([
+        {"title": "Evento demo", "start": "2025-10-10T10:00", "end": "2025-10-10T11:00", "draft": False}
+    ])'''
     return jsonify(real + draft + g_events)
 
 
